ad617f3cb1f8d7109aaf9aab2dad46b12f859771
krsna <krsna@KRSNA>
2022-07-11 15:50:16 +0530

    Add project files.

---
+++ InventaryManagementSystem/Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿//using InventaryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventaryManagementSystem.Models;
+
+namespace InventaryManagementSystem.Controllers
+{
+    public class ProductController : Controller
+    {
+        Inventory_managementEntities11 db = new Inventory_managementEntities11();
+        // GET: Product
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+
+        public ActionResult DisplayProduct()
+        {
+            List<Product> list= db.Products.OrderBy(x=> x.id).ToList();
+            return View(list);
+        }
+         
+
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateProduct(Product pro)
+        {
+            db.Products.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
+        
+        [HttpGet]
+        public ActionResult UpdateProduct(int id)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pr);
+        }
+
+        [HttpPost]
+
+        public ActionResult UpdateProduct(int id, Product pro)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            pr.Product_name = pro.Product_name;
+            pr.Product_qnty = pro.Product_qnty;
+            db.SaveChanges();
+
+            return RedirectToAction("DisplayProduct");
+        }
+
+        [HttpGet]
+        public ActionResult ProductDetails(int id)
+        {
+            Product pr= db.Products.Where(x => x.id == id).SingleOrDefault();   
+            return View(pr);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteProduct(int id)
+        {
+            Product pro = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pro);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteProduct(int id, Product pro)
+        {
+            Product p= db.Products.Where(x => x.id == id).SingleOrDefault();
+            db.Products.Remove(p);
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
+
+
+    }
+}
---
+++ InventaryManagementSystem/Controllers/SaleController.cs
@@ -0,0 +1,86 @@
+﻿using InventaryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace InventaryManagementSystem.Controllers
+{
+    public class SaleController : Controller
+    {
+        Inventory_managementEntities11 db = new Inventory_managementEntities11();        // GET: Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisplaySale()
+        {
+            List<Sale> list = db.Sales.OrderByDescending(x => x.id).ToList();
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult SaleProduct()
+        {
+            List<string> list = db.Products.Select(x => x.Product_name).ToList();
+            ViewBag.ProductName = new SelectList(list);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SaleProduct(Sale S)
+        {
+            db.Sales.Add(S);
+            db.SaveChanges();
+            return RedirectToAction("DisplaySale");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Sale s = db.Sales.Where(x => x.id == id).SingleOrDefault();
+            List<string> list = db.Products.Select(x => x.Product_name).ToList();
+            ViewBag.ProductName = new SelectList(list);
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Sale S)
+        {
+            Sale s = db.Sales.Where(x => x.id == id).SingleOrDefault();
+            s.Sale_prod = S.Sale_prod;
+            s.Sale_qnty = S.Sale_qnty;
+            s.Sale_date = S.Sale_date;
+            db.SaveChanges();
+            return RedirectToAction("DisplaySale");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Sale s = db.Sales.Where(x => x.id == id).SingleOrDefault();
+
+            return View(s);
+        }
+
+        [HttpGet]
+
+        public ActionResult Delete(int id)
+        {
+            Sale s = db.Sales.Where(x => x.id == id).SingleOrDefault();
+            //List<string> list = db.Products.Select(x => x.Product_name).ToList();
+            //ViewBag.ProductName = new SelectList(list);
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Sale S)
+        {
+            Sale s = db.Sales.Where(x => x.id == id).SingleOrDefault();
+            db.Sales.Remove(s);
+            db.SaveChanges();
+            return RedirectToAction("DisplaySale");
+
+        }
+    }
+}
